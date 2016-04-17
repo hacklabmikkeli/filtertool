d70dfac85d24987fbd73b9a76dfcdc95b9db67cd
ilmo.euro <ilmo.euro@gmail.com>
2016-04-17 16:39:51 +0300

    ** Resilience to errors

--- response-plotter.py
+++ response-plotter.py
@@ -32,19 +32,27 @@ def get_data():
     sock = socket.socket()
     sock.connect(('10.0.0.3', 3001))
     sock.sendall(b"STARTBIN")
+    sock.settimeout(0.1)
     data = b''
-    while True:
-        recvd = sock.recv(4096)
-        data = data + recvd
-        if len(recvd) < 4096:
-            break
+    try:
+        while True:
+            recvd = sock.recv(4096)
+            data = data + recvd
+    except socket.timeout:
+        pass
     sock.close()
     return data
 
 
 def parse_data(data):
+    if len(data) < 80:
+        data = b'\0' * 80
     header = data[:0x50]
     payload = data[0x51:]
+    print(len(payload))
+
+    if len(payload) % 2 == 1:
+        payload = payload[:-1]
 
     (time_div_encoded, volts_div_encoded) = struct.unpack(HEADER_FMT, header)
     time_div = (1,2,4)[time_div_encoded % 3] * 10 ** (time_div_encoded // 3)
@@ -55,7 +63,11 @@ def parse_data(data):
 
 
 def calculate_fft(signal):
-    fft_signal = rfft(signal)
+    length = len(signal)
+    signal_2 = concatenate((signal, zeros(length*9)))
+    if all(signal_2 == zeros(len(signal_2))):
+        signal_2 = array([1, 0, 0, 0])
+    fft_signal = rfft(signal_2)[:length//10]
     return (abs(fft_signal), angle(fft_signal))
 
 
@@ -68,7 +80,7 @@ class FilterTool:
         self.phase_subplot = self.figure.add_subplot(3,1,3)
 
     def run(self):
-        ani = animation.FuncAnimation(self.figure, self._run_once, interval=1000)
+        ani = animation.FuncAnimation(self.figure, self._run_once, interval=250)
         show()
 
     def _run_once(self, i):
